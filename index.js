8c70daf3d5b9d4c110abf119da903e359045d2d3
Handa <anandamuhammadmtq@gmail.com>
2020-11-18 18:04:24 +0800

    Initial files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express(); 
+
+// Membuat handler untuk endpoint,
+// dalam hal ini endpointnya "/"
+app.get("/", (req, res) => {
+  res.send("<h1>Selamat datang</h1>");
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server running at port 3000");
+});
