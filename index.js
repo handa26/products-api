efee86dedee32cf1a89f95ccaf2c6133e8ffc6d0
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-11-20 15:42:27 +0800

    Minor change

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const app = express();
 // Menambahkan logger
 app.use(logger("dev"));
 
-// Menambahkan bodyparser untuk x-www-form-urlencoded
+// Bodyparser
+// Parser untuk x-www-form-urlencoded
 app.use(express.urlencoded({
   extended: false
 }));
-
-// menambahkan parser untuk raw json
-app.use(express.json());
+app.use(express.json()); // parser untuk raw json
 
 // Router
 app.use("/", require("./src/routes/index"));
@@ -20,10 +19,8 @@ app.use("/products", require("./src/routes/products"));
 app.use("/product", require("./src/routes/product"));
 app.use("/search", require("./src/routes/search"));
 
-// req params dan query
+const PORT = 3000;
 
-const port = 3000;
-
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("Server running at port 3000");
 });
