785e555111c561141557395793e085a899ceb9cc
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-11-30 23:17:38 +0800

    Combine search product & category into one route

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,21 +1,30 @@
 const searchModel = require("../models/search");
 const form = require("../helpers/form");
 
+
 module.exports = {
   searchProduct: (req, res) => {
-    const { q } = req.query;
-    const keyword = `%${q}%`;
+    const { q, c } = req.query;
+    const keyword = ["%" + q + "%", "%" + c + "%"];
     searchModel
       .searchProduct(keyword)
-      .then((data) => form.success(res, data))
-      .catch((err) => form.error(res, err));
+      .then((data) => {
+        data.length
+          ? form.success(res, data)
+          : res.status(404).json({ msg: "Data not Found" });
+        // res.json(data);
+      })
+      .catch((err) => {
+        form.error(res, err);
+        // res.status(500).json(err);
+      });
   },
-  searchByCategory: (req, res) => {
-    const { c } = req.query;
-    const keyword = `%${c}%`;
-    searchModel
-      .searchByCategory(keyword)
-      .then((data) => form.success(res, data))
-      .catch((err) => form.error(res, err));
-  },
+  // searchByCategory: (req, res) => {
+  //   const { c } = req.query;
+  //   const keyword = `%${c}%`;
+  //   searchModel
+  //     .searchByCategory(keyword)
+  //     .then((data) => form.success(res, data))
+  //     .catch((err) => form.error(res, err));
+  // },
 };
--- src/models/search.js
+++ src/models/search.js
@@ -4,7 +4,7 @@ module.exports = {
   searchProduct: (keyword) => {
     return new Promise((resolve, reject) => {
       const queryString =
-        "SELECT p.id, p.product_name, p.product_brand, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE p.product_name LIKE ?";
+        "SELECT p.id, p.product_name, p.product_brand, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE p.product_name LIKE ? OR category_name LIKE ?";
       db.query(queryString, keyword, (err, data) => {
         if (!err) {
           resolve(data);
