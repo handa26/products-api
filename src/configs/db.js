514dd6e097c6fd3a4a910bfd33fcd201b9dc098f
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-12-04 14:31:08 +0800

    Add dotenv

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,11 @@
 const mySQL = require("mysql");
+const { HOST, USER, PASSWORD, DATABASE } = process.env;
 
 const db = mySQL.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "products",
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  database: DATABASE,
 });
 
 db.connect((err) => {
