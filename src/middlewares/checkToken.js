7ba6157cc392a82d9bfbc6f2a625ef66c1418d25
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-12-09 16:20:30 +0800

    Authorization using bearer

---
+++ src/middlewares/checkToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const form = require("../helpers/form");
+
+module.exports = (req, res, next) => {
+  const { headers } = req;
+  const bearerToken = req.header("x-access-token");
+  if (!bearerToken) {
+    form.error(res, {
+      msg: "Please Login First",
+      status: 401,
+    });
+  } else {
+    // Bearer Token
+    const token = bearerToken.split(" ")[1];
+    try {
+      const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+      req.decodedToken = decodedToken;
+
+      next();
+    } catch (error) {
+      form.error(res, {
+        msg: "Invalid token",
+        error,
+        status: 401,
+      });
+    }
+  }
+}
