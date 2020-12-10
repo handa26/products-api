baf60875a0b05e1db57512ad823c5de6168e0368
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-12-10 16:36:44 +0800

    Paginate for search products

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -4,9 +4,18 @@ const form = require("../helpers/form");
 module.exports = {
   searchProduct: (req, res) => {
     const { name, category, sort } = req.query;
-    let {sortby} = req.query;
+    let { sortby } = req.query;
     const searchName = `%${name}%`;
     const searchCategory = `%${category}%`;
+
+    const { query } = req;
+    const limit = Number(query.limit) || 4;
+    const page = Number(query.page) || 1;
+    // limit  = 5
+    // page   = 1 2 3  4
+    // offset = 0 5 10 15
+    // offset = (page-1)*limit
+    const offset = (page - 1) * limit || 0;
 
     if (sortby === "name") {
       sortby = "product_name";
@@ -14,34 +23,58 @@ module.exports = {
       sortby = "created_at";
     }
 
-    if (name !== undefined && category !== undefined) {
-      res.status(500).json({
-        status: "error",
-        message: "u cant find more column at a same time",
-      });
-    } else {
-      if (name) {
-        searchModel
-          .searchProduct(searchName, sortby, sort)
-          .then((data) => {
-            form.success(res, data);
-          })
-          .catch((err) => {
-            form.error(res, err);
-          });
-      }
-
-      if (category) {
-        searchModel
-          .searchByCategory(searchCategory, sortby, sort)
-          .then((data) => {
-            form.success(res, data);
-            console.log(searchCategory);
-          })
-          .catch((err) => {
-            form.error(res, err);
-          });
-      }
+    if (name) {
+      searchModel
+        .searchProduct(searchName, sortby, sort, limit, offset)
+        .then((data) => {
+          if (data.length === 0) {
+            const emptyResult = {
+              status: 404,
+              products: "Page not found",
+            };
+            res.json(emptyResult);
+          }
+          const newResult = {
+            products: data,
+            pageInfo: {
+              currentPage: page,
+              previousPage:
+                page === 1 ? null : `&page=${page - 1}&limit=${limit}`,
+              nextPage: `&page=${page + 1}&limit=${limit}`,
+            },
+          };
+          res.json(newResult);
+        })
+        .catch((err) => {
+          form.error(res, err);
+        });
     }
-  }
+
+    if (category) {
+      searchModel
+        .searchByCategory(searchCategory, sortby, sort, limit, offset)
+        .then((data) => {
+          if (data.length === 0) {
+            const emptyResult = {
+              status: 404,
+              products: "Page not found",
+            };
+            res.json(emptyResult);
+          }
+          const newResult = {
+            products: data,
+            pageInfo: {
+              currentPage: page,
+              previousPage:
+                page === 1 ? null : `&page=${page - 1}&limit=${limit}`,
+              nextPage: `&page=${page + 1}&limit=${limit}`,
+            },
+          };
+          res.json(newResult);
+        })
+        .catch((err) => {
+          form.error(res, err);
+        });
+    }
+  },
 };
--- src/models/search.js
+++ src/models/search.js
@@ -1,13 +1,14 @@
 const db = require("../configs/db");
 
 module.exports = {
-  searchProduct: (keyword, sortBy, sort) => {
+  searchProduct: (keyword, sortBy, sort, limit, offset) => {
     return new Promise((resolve, reject) => {
       let qs = '';
       if (sortBy) {
-        qs = `SELECT p.id, p.product_name, p.product_brand, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE p.product_name LIKE ? ORDER BY ${sortBy} ${sort}`
+        qs = `SELECT p.id, p.product_name, p.product_brand, p.image, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE p.product_name LIKE ? ORDER BY ${sortBy} ${sort} LIMIT ${limit} OFFSET ${offset}`;
       } else {
-        qs = 'SELECT p.id, p.product_name, p.product_brand, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE p.product_name LIKE ?'
+        qs =
+          "SELECT p.id, p.product_name, p.product_brand, p.image, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE p.product_name LIKE ?";
       }
 
       db.query(qs, keyword, (err, data) => {
@@ -19,14 +20,14 @@ module.exports = {
       });
     });
   },
-  searchByCategory: (keyword, sortBy, sort) => {
+  searchByCategory: (keyword, sortBy, sort, limit, offset) => {
     return new Promise((resolve, reject) => {
       let qs = '';
       if (sortBy) {
-        qs = `SELECT p.id, p.product_name, p.product_brand, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE c.category_name LIKE ? ORDER BY ${sortBy} ${sort}`;
+        qs = `SELECT p.id, p.product_name, p.product_brand, p.image, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE c.category_name LIKE ? ORDER BY ${sortBy} ${sort} LIMIT ${limit} OFFSET ${offset}`;
       } else {
         qs =
-          "SELECT p.id, p.product_name, p.product_brand, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE c.category_name LIKE ?";
+          "SELECT p.id, p.product_name, p.product_brand, p.image, p.product_description, p.product_price, c.category_name, p.product_color, p.size,p.product_rating, p.product_qty FROM items AS p JOIN categories AS c ON c.id = p.category_id WHERE c.category_name LIKE ?";
       }
 
       db.query(qs, keyword, (err, data) => {
